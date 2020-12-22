a20caf15b63951a644876b4ebe418d2c3b4b239e
mattkachek <mkachek@icloud.com>
2020-12-21 21:32:30 -0600

    Day 19 plus interface changes

---
+++ AdventOfCode/Day19/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Day19
+{
+    public class Program
+    {
+        public void Select()
+        {
+            Console.WriteLine("Choose which version to run");
+            Console.WriteLine("1 - Part 1");
+            Console.WriteLine("2 - Part 2");
+            var input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    Console.WriteLine(Part1());
+                    break;
+                case "2":
+                    Console.WriteLine(Part2());
+                    break;
+
+                default:
+                    Console.WriteLine("Default Case");
+                    break;
+            }
+        }
+
+        public string Part1()
+        {
+            var fullInput = GetInput();
+            var rulesInput = fullInput.TakeWhile(s => s!="");
+            var messageInput = fullInput.SkipWhile(s => s != "").Skip(1);
+
+            var ruleDict = new Dictionary<int, Tuple<string, string>>();
+            foreach(string rule in rulesInput)
+            {
+                var ruleNum = int.Parse(rule.Split(':').First());
+                var ruleOne = rule.Split(new char[] { ':', '|' })[1].Trim();
+                var ruleTwo = "";
+                if (rule.Contains('|'))
+                {
+                    ruleTwo = rule.Split(new char[] { ':', '|' })[2].Trim();
+                }
+                ruleDict.Add(ruleNum, new Tuple<string, string>(ruleOne, ruleTwo));
+            }
+
+            var matchCount = 0;
+            var validMsgs = GetSubRuleStrings(ruleDict, 0);
+            foreach(string message in messageInput)
+            {
+                if(validMsgs.Contains(message))
+                {
+                    matchCount++;
+                }
+            }
+
+            return matchCount.ToString();
+        }
+
+        public string Part2()
+        {
+            var fullInput = GetInput();
+            var rulesInput = fullInput.TakeWhile(s => s != "");
+            var messageInput = fullInput.SkipWhile(s => s != "").Skip(1);
+
+            var ruleDict = new Dictionary<int, Tuple<string, string>>();
+            foreach (string rule in rulesInput)
+            {
+                var ruleNum = int.Parse(rule.Split(':').First());
+                var ruleOne = rule.Split(new char[] { ':', '|' })[1].Trim();
+                var ruleTwo = "";
+                if (rule.Contains('|'))
+                {
+                    ruleTwo = rule.Split(new char[] { ':', '|' })[2].Trim();
+                }
+                ruleDict.Add(ruleNum, new Tuple<string, string>(ruleOne, ruleTwo));
+            }
+
+            var matchCount = 0;
+            var validMsgs = GetSubRuleStrings(ruleDict, 0);
+            var prefixes = GetSubRuleStrings(ruleDict, 42);
+            var suffixes = GetSubRuleStrings(ruleDict, 31);
+            var invalidMsgs = new List<string>();
+            var doubleInvalidMsgs = new List<string>();
+
+            foreach (string message in messageInput)
+            {
+                if (validMsgs.Contains(message))
+                {
+                    matchCount++;
+                }
+                else
+                {
+                    invalidMsgs.Add(message);
+                }
+            }
+
+            foreach(string message in invalidMsgs)
+            {
+                var substrings = new List<string>();
+                var stillValid = true;
+                for(int i=0; i<message.Length; i+=8)
+                {
+                    substrings.Add(message.Substring(i, 8));
+                }
+                if (suffixes.Contains(substrings.Last()))
+                {
+                    while (stillValid && substrings.Count() > 2 && suffixes.Contains(substrings.Last()))
+                    {
+                        if (prefixes.Contains(substrings.First()))
+                        {
+                            substrings.RemoveAt(0);
+                            substrings.RemoveAt(substrings.Count() - 1);
+                        }
+                        else
+                        {
+                            stillValid = false;
+                        }
+
+                    }
+                    if (substrings.All(s => prefixes.Contains(s)))
+                    {
+                        matchCount++;
+                    }
+                }
+            }
+
+            return matchCount.ToString();
+        }
+
+        private List<string> GetSubRuleStrings(Dictionary<int, Tuple<string, string>> ruleDict, int ruleIndex)
+        {
+            var currentStringList = new List<string>();
+            if(ruleDict[ruleIndex].Item1.Contains('\"'))
+            {
+                currentStringList.Add(ruleDict[ruleIndex].Item1.Replace("\"", string.Empty));
+            }
+            else
+            {
+                var paths1 = ruleDict[ruleIndex].Item1.Split(' ').Select(s => int.Parse(s)).ToList();
+                var subList1 = GetSubRuleStrings(ruleDict, paths1[0]);
+                var subList2 = new List<string>();
+                subList2.Add("");
+                if(paths1.Count() > 1)
+                {
+                    subList2 = GetSubRuleStrings(ruleDict, paths1[1]);
+                }
+
+                foreach(string sl1 in subList1)
+                {
+                    foreach(string sl2 in subList2)
+                    {
+                        currentStringList.Add(sl1 + sl2);
+                    }
+                }
+
+
+                var paths2 = new List<int>();
+                if (ruleDict[ruleIndex].Item2 != "")
+                {
+                    paths2 = ruleDict[ruleIndex].Item2.Split(' ').Select(s => int.Parse(s)).ToList();
+                    subList1 = GetSubRuleStrings(ruleDict, paths2[0]);
+                    subList2 = new List<string>();
+                    subList2.Add("");
+                    if (paths2.Count() > 1)
+                    {
+                        subList2 = GetSubRuleStrings(ruleDict, paths2[1]);
+                    }
+
+                    foreach (string sl1 in subList1)
+                    {
+                        foreach (string sl2 in subList2)
+                        {
+                            currentStringList.Add(sl1 + sl2);
+                        }
+                    }
+                }
+
+            }
+            return currentStringList;
+        }
+
+        private List<string> GetInput()
+        {
+            var filePath = @"C:\Users\mkach\source\repos\AdventOfCode2020\AdventOfCode\Day19\Input.txt";
+            List<string> values = new List<string>();
+
+            using (StreamReader sr = File.OpenText(filePath))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    values.Add(s);
+                }
+            }
+            return values;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -10,91 +10,104 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Choose Day to Run");
-            var day = Console.ReadLine();
-            
-            switch(day)
+            var repeat = true;
+            while(repeat)
             {
-                case "1":
-                    var Day1 = new Day1.Program();
-                    Day1.Select();
-                    break;
-                case "2":
-                    var Day2 = new Day2.Program();
-                    Day2.Select();
-                    break;
-                case "3":
-                    var Day3 = new Day3.Program();
-                    Day3.Select();
-                    break;
-                case "4":
-                    var Day4 = new Day4.Program();
-                    Day4.Select();
-                    break;
-                case "5":
-                    var Day5 = new Day5.Program();
-                    Day5.Select();
-                    break;
-                case "6":
-                    var Day6 = new Day6.Program();
-                    Day6.Select();
-                    break;
-                case "7":
-                    var Day7 = new Day7.Program();
-                    Day7.Select();
-                    break;
-                case "8":
-                    var Day8 = new Day8.Program();
-                    Day8.Select();
-                    break;
-                case "9":
-                    var Day9 = new Day9.Program();
-                    Day9.Select();
-                    break;
-                case "10":
-                    var Day10 = new Day10.Program();
-                    Day10.Select();
-                    break;
-                case "11":
-                    var Day11 = new Day11.Program();
-                    Day11.Select();
-                    break;
-                case "12":
-                    var Day12 = new Day12.Program();
-                    Day12.Select();
-                    break;
-                case "13":
-                    var Day13 = new Day13.Program();
-                    Day13.Select();
-                    break;
-                case "14":
-                    var Day14 = new Day14.Program();
-                    Day14.Select();
-                    break;
-                case "15":
-                    var Day15 = new Day15.Program();
-                    Day15.Select();
-                    break;
-                case "16":
-                    var Day16 = new Day16.Program();
-                    Day16.Select();
-                    break;
-                case "17":
-                    var Day17 = new Day17.Program();
-                    Day17.Select();
-                    break;
-                case "18":
-                    var Day18 = new Day18.Program();
-                    Day18.Select();
-                    break;
+                Console.WriteLine("Choose Day to Run");
+                Console.WriteLine("Press q to quit");
+                var day = Console.ReadLine();
+            
+                switch(day)
+                {
+                    case "1":
+                        var Day1 = new Day1.Program();
+                        Day1.Select();
+                        break;
+                    case "2":
+                        var Day2 = new Day2.Program();
+                        Day2.Select();
+                        break;
+                    case "3":
+                        var Day3 = new Day3.Program();
+                        Day3.Select();
+                        break;
+                    case "4":
+                        var Day4 = new Day4.Program();
+                        Day4.Select();
+                        break;
+                    case "5":
+                        var Day5 = new Day5.Program();
+                        Day5.Select();
+                        break;
+                    case "6":
+                        var Day6 = new Day6.Program();
+                        Day6.Select();
+                        break;
+                    case "7":
+                        var Day7 = new Day7.Program();
+                        Day7.Select();
+                        break;
+                    case "8":
+                        var Day8 = new Day8.Program();
+                        Day8.Select();
+                        break;
+                    case "9":
+                        var Day9 = new Day9.Program();
+                        Day9.Select();
+                        break;
+                    case "10":
+                        var Day10 = new Day10.Program();
+                        Day10.Select();
+                        break;
+                    case "11":
+                        var Day11 = new Day11.Program();
+                        Day11.Select();
+                        break;
+                    case "12":
+                        var Day12 = new Day12.Program();
+                        Day12.Select();
+                        break;
+                    case "13":
+                        var Day13 = new Day13.Program();
+                        Day13.Select();
+                        break;
+                    case "14":
+                        var Day14 = new Day14.Program();
+                        Day14.Select();
+                        break;
+                    case "15":
+                        var Day15 = new Day15.Program();
+                        Day15.Select();
+                        break;
+                    case "16":
+                        var Day16 = new Day16.Program();
+                        Day16.Select();
+                        break;
+                    case "17":
+                        var Day17 = new Day17.Program();
+                        Day17.Select();
+                        break;
+                    case "18":
+                        var Day18 = new Day18.Program();
+                        Day18.Select();
+                        break;
+                    case "19":
+                        var Day19 = new Day19.Program();
+                        Day19.Select();
+                        break;
 
-                default:
-                    Console.WriteLine("Default Case");
-                    break;
+
+                    case "q":
+                        repeat = false;
+                        break;
+                    default:
+                        Console.WriteLine("Default Case");
+                        break;
+                }
             }
 
-            Console.Write("\nPress any key to exit...");
-            Console.ReadKey(true);
+            //Console.Write("\nPress any key to exit...");
+            //Console.ReadKey(true);
 
         }
     }
